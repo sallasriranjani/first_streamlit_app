337cba28bb19d36f6bb083fb8e4549d38d7383c0
sallasriranjani <135412573+sallasriranjani@users.noreply.github.com>
2023-06-04 15:04:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,23 +33,3 @@ try:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
         streamlit.dataframe(fruityvice_normalized)
-
-
-# write your own comment -what does the next line do? 
-
-# write your own comment - what does this do?
-
-
-#streamlit.stop()
-
-
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-#my_data_rows = my_cur.fetchall()
-#streamlit.header("Fruit Load List Contains :")
-#streamlit.dataframe(my_data_rows)
-
-#add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
